7476de46f30e4a8005d0e1c447294cb8ff643102
Somodi Károly Ákos <charlssom@gmail.com>
2016-02-13 15:16:31 +0100

    Readme change

---
+++ geometry.py
@@ -0,0 +1,33 @@
+import math
+
+QUIT = 15749
+
+
+# circle perimeter
+def circle(radius):
+    return radius * math.pi * 2
+
+
+# circle area
+def circle_area(radius):
+    return radius ** 2 * math.pi
+
+
+# rectangle perimeter
+def rectangle_perimeter(side_a, side_b):
+    return 2 * (side_a + side_b)
+
+
+# rectangle area
+def rectangle_area(side_a, side_b):
+    return side_a * side_b
+
+
+# square perimeter
+def square_perimeter(side):
+    return side * 4
+
+
+# square area
+def square_area(side):
+    return side ** 2
--- main.py
+++ main.py
@@ -1,10 +1,64 @@
-import math
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# import geometry
+# import sys
+# from PyQt4 import QtGui
 
 
-def circle(radius):
-    return radius * math.pi * 2
+# def main():
+#
+#     app = QtGui.QApplication(sys.argv)
+#
+#     w = QtGui.QWidget()
+#     w.resize(250, 150)
+#     w.move(300, 300)
+#     w.setWindowTitle('Simple')
+#     w.show()
+#
+#     sys.exit(app.exec_())
+#
+#
+# if __name__ == '__main__':
+#     main()
+#
+# def main():
+#     r = float(input("Enter the radius please:"))
+#     x = geometry.circle_area(r)
+#     print(x)
+#     side = float(input("Enter the square side please:"))
+#     x = geometry.square_perimeter(side)
+#     print(x)
+#
+# main()
 
 
-r = float(input("Enter the radius please:"))
-x = circle(r)
-print(x)
+import pygame
+
+pygame.init()
+
+size = (700, 500)
+surface = pygame.display.set_mode(size)
+pygame.display.set_caption("My game")
+
+end = False
+orange = (255, 167, 43)
+gold = (250, 237, 50)
+blue = (31, 176, 224)
+
+while not end:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            end = True
+
+    surface.fill(orange)
+    pygame.draw.circle(surface, gold, [350, 150], 120, 0)
+    pygame.draw.rect(surface, blue, [0, 320, 700, 180], 0)
+    #   ha le va nyomva akkor menjen is elorre
+
+    #   rajzoljuk ki
+
+    #   takaritsunk
+    pygame.display.flip()
+
+pygame.quit()
